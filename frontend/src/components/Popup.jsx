4b92279d4b00728bd9eb01617711110671039db7
Abdo Belhaje <abdelkodoussebelhaje@gmail.com>
2024-02-25 15:04:47 +0100

    add the popup for private room

---
+++ frontend/src/components/Popup.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react'
+import { useWebSocket } from '../websocket/WebSocketProvider';
+
+function Popup() {
+  const { webSocketConnection, setPopup , DisplayPopup} = useWebSocket();
+  const inputRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
+  const Code = Array.from({ length: 4 });
+  
+
+  const handleInputChange = (index, e) => {
+    const value = e.target.value;
+    // Move focus to the next input when a digit is entered
+    Code[index] = value
+    if (value.length === 1 && index < inputRefs.length - 1) {
+      inputRefs[index + 1].current.focus();
+    }
+    else if (index === inputRefs.length - 1){
+      setPopup(false)
+      if(webSocketConnection && Code.join("") === "0258"){
+          webSocketConnection.send("/app/private-room",{},"ON")
+      }
+      
+    }
+    // You can add additional logic here, such as handling the entered code
+  };
+
+  return (
+    <div class="private-room_popup" onClick={()=> setPopup(false)}>
+        <div className="code" onClick={(e)=> e.stopPropagation()}>
+            <div className="EnterCode">Enter Code</div>
+            <div className="Inputs">
+                {inputRefs.map((ref, index) => (
+                  <input
+                    key={index}
+                    type="text"
+                    maxLength="1"
+                    ref={ref}
+                    onChange={(e) => handleInputChange(index, e)}
+                    
+                  />
+                ))}
+              
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Popup
