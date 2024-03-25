64240c3dbba18dd19bf45de9ce1f81bade939093
Abdo Belhaje <abdelkodoussebelhaje@gmail.com>
2024-03-25 21:19:52 +0000

    change

--- backend/src/main/java/com/example/backend/config/MqttConfig.java
+++ backend/src/main/java/com/example/backend/config/MqttConfig.java
@@ -89,7 +89,6 @@ public class MqttConfig {
                                                     + jsonNode.get("humidity").asLong()));
                         }
                     } catch (Exception e) {
-                        // View exception msg
                         System.out.println(e.getMessage());
                     }
                     messagingTemplate.convertAndSend("/topic/data", payload);
