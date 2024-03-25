5926fd2876005b09f7a6b0d842f17d41d741fa39
Abdo Belhaje <abdelkodoussebelhaje@gmail.com>
2024-03-25 19:25:24 +0000

    update SMS service

--- backend/src/main/java/com/example/backend/service/impl/SmsServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/impl/SmsServiceImpl.java
@@ -13,8 +13,8 @@ import com.twilio.rest.api.v2010.account.Message;
 @Service
 public class SmsServiceImpl implements SmsService {
 
-    private String ACCOUNT_SID = "ACb47e0e62aef82cada008cb3831a09898";
-    private String AUTH_TOKEN = "9227476ab940037b43c3a1c3c4e9a8a3";
+    private static String ACCOUNT_SID = "ACb47e0e62aef82cada008cb3831a09898";
+    private static String AUTH_TOKEN = "5d8758be95d170ca6daaf75f28a725c5";
 
     public SmsServiceImpl() {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
