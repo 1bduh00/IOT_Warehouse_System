bfb94835f887e947655c7a70d8bf6355ace27e91
Abdo Belhaje <abdelkodoussebelhaje@gmail.com>
2024-04-01 14:59:06 +0000

    catch undefined values

--- frontend/src/page/Dashboard.jsx
+++ frontend/src/page/Dashboard.jsx
@@ -22,7 +22,7 @@ function Dashboard() {
             await establishConnection();
           };
           StartConnection()
-    }, []);
+      }, []);
 
     useEffect(() => {
         // console.log('WebSocket connection is established and ready:', webSocketConnection);
@@ -46,14 +46,14 @@ function Dashboard() {
                 <Header />
             <div className='h-full w-full'>
                 <div className="flex justify-between px-12 py-3 flex-wrap">
-                    <SensorsData title="Humidity" prctg={sensorData && sensorData.humidity ? sensorData.humidity : "0"} img={humidity} />
+                    <SensorsData title="Humidity" prctg={sensorData ? sensorData.humidity : "0"} img={humidity} />
                     <SensorsData title="Employees" prctg={sensorData ? sensorData.employees : "0"} img={employees} />
                     <SensorsData title="Ventilator" button="yes" img={Ventilator} />
                     <SensorsData title="private" button="yes" img={protect} />
                 </div>
                 <div className="h-full md:h-3/4 gap-y-4 gap-x-12 px-12 grid-cols-1 grid md:grid-cols-5 md:grid-rows-2 ">
                     <Notif data={sensorData? sensorData : ""} />
-                    <Temperature value={sensorData && sensorData.temperature ? sensorData.temperature : "0"}/>
+                    <Temperature value={sensorData ? sensorData.temperature : "0"}/>
                     <RacsComd />
                     <Racs rac1={sensorData ? sensorData.Rac1 : 0} rac2={sensorData ? sensorData.Rac2 : 0} />
 
